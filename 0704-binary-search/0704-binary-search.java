441158f3c97ac570ce632660eaecb8f81680c0c2
Nikita Nagda <nikitanagda196@gmail.com>
2026-09-07 08:14:24 +0530

    Time: 3 ms (0.76%), Space: 48.5 MB (13.97%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        Arrays.sort(nums);
+        //int avg=target/2;
+        int left=0;
+        int right=nums.length-1;
+        while(left<=right){
+             int mid=left+(right-left)/2;
+            if(target==nums[mid]) return mid;
+            else if(nums[mid]<target){
+                left=mid+1;
+            }
+            else {
+                right=mid-1;
+            }
+        }
+        //if(left==right) return -1;
+        return -1;
+        //for(int )
+    }
+}
